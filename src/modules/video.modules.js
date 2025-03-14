93bfb0dea36f518b9a5bfadf4579220b850ca03f
aniiketsinghh <aniketsingh2004a@gmail.com>
2025-03-14 22:33:39 +0530

    adding user and video module

---
+++ src/modules/video.modules.js
@@ -0,0 +1,49 @@
+import mongoose,{Schema} from 'mongoose'
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+const videoSchema= new Schema(
+    {
+        id:{
+            type: String,
+            require: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index:true
+        },
+        videofile:{
+            type: String,
+            require: true,
+        },
+        thumbnail:{
+            type: String,
+            require: true,
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        },
+        title:{
+            type: String,
+            require: true,
+        },
+        discription:{
+            type: String,
+        },
+        duration:{
+            type: Number,
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        isPublished:{
+            type: Boolean,
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video= Schema.model("Video",videoSchema);
