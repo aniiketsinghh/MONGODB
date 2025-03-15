0b0fd12fce8a4e62a285e0e935afabdb20167749
aniiketsinghh <aniketsingh2004a@gmail.com>
2025-03-15 11:05:19 +0530

    user model updated

--- src/modules/user.modules.js
+++ src/modules/user.modules.js
@@ -55,12 +55,12 @@ const userSchema=new Schema({
 userSchema.pre("save",async function (next){
     if(!this.isModified('password'))return next();
 
-    this.password=bcrypt.hash(this.password,10)
+    this.password= await bcrypt.hash(this.password,10)
     next()
 })
 
-userSchema.methods.isPasswordCorrect=async function(password){
-    return await bcrypt.compare(password,this.password)
+userSchema.methods.isPasswordCorrect=async function(password){ //for login it will check the pass if it write then true and enter else pass is incorrect!
+    return await bcrypt.compare(password,this.password) 
 }
 
 userSchema.methods.generateAccessToken=function(){
