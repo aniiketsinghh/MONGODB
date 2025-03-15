0e86e099556d2d36a7bb5ea7ad6ff250e3936d85
aniiketsinghh <aniketsingh2004a@gmail.com>
2025-03-15 21:54:12 +0530

    added routes

--- src/utils/aysncHandler.js
+++ src/utils/aysncHandler.js
@@ -1,8 +1,9 @@
 // using next as a middleware ! your kackend connect with your frontend thats why you are adding that next , its like a checker or if else condition if its true then okk else won't connect
 
 const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next))
         .catch((error)=>next(error))
     }
 }
+export {asyncHandler}
